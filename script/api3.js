129a09595b0a02c2fbd7f240ae1cede1a7220c0c
Jubayer husain <jubayerhusai360@gmail.com>
2024-10-01 19:37:22 +0600

    api3 done with onclick

---
+++ script/api3.js
@@ -0,0 +1,13 @@
+
+const allPost = () =>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => postSection(data));
+}
+
+function postSection(post) {
+    for(const data of post){
+        // console.log(data)
+        console.log(data.title)
+    }
+}
